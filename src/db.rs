1f4c511c68549f8dc03ae47c84188e3c90058c6d
clshu <clshu2020@gmail.com>
2023-08-26 22:11:58 -0700

    RJ-6 Add CRUD operations for Epics/Stories

--- src/db.rs
+++ src/db.rs
@@ -1,7 +1,92 @@
-use anyhow::Result;
+use anyhow::{anyhow, Result};
 use std::fs;
 
 use crate::models::{DBState, Epic, Status, Story};
+
+pub struct JiraDatabase {
+    database: Box<dyn Database>,
+}
+
+impl JiraDatabase {
+    pub fn new(file_path: String) -> Self {
+        Self {
+            database: Box::new(JSONFileDatabase { file_path }),
+        }
+    }
+
+    pub fn read_db(&self) -> Result<DBState> {
+        self.database.read_db()
+    }
+
+    pub fn create_epic(&self, epic: Epic) -> Result<u32> {
+        let mut db_state = self.read_db()?;
+        let epic_id = db_state.last_item_id + 1;
+        db_state.last_item_id = epic_id;
+        db_state.epics.insert(epic_id, epic);
+        self.database.write_db(&db_state)?;
+        Ok(epic_id)
+    }
+
+    pub fn create_story(&self, story: Story, epic_id: u32) -> Result<u32> {
+        let mut db_state = self.read_db()?;
+        let story_id = db_state.last_item_id + 1;
+        db_state.last_item_id = story_id;
+        db_state.stories.insert(story_id, story);
+        db_state
+            .epics
+            .get_mut(&epic_id)
+            .ok_or_else(|| anyhow!(format!("Can not find epic id {epic_id:?}")))?
+            .stories
+            .push(story_id);
+        self.database.write_db(&db_state)?;
+        Ok(story_id)
+    }
+
+    pub fn delete_epic(&self, epic_id: u32) -> Result<()> {
+        let mut db_state = self.read_db()?;
+        db_state.epics.remove(&epic_id);
+        self.database.write_db(&db_state)?;
+        Ok(())
+    }
+
+    pub fn delete_story(&self, epic_id: u32, story_id: u32) -> Result<()> {
+        let mut db_state = self.read_db()?;
+        db_state.stories.remove(&story_id);
+        db_state
+            .epics
+            .get_mut(&epic_id)
+            .ok_or_else(|| anyhow!(format!("Can not find epic id {epic_id:?}")))?
+            .stories
+            .remove(story_id as usize);
+        self.database.write_db(&db_state)?;
+
+        Ok(())
+    }
+
+    pub fn update_epic_status(&self, epic_id: u32, status: Status) -> Result<()> {
+        let mut db_state = self.read_db()?;
+        db_state
+            .epics
+            .get_mut(&epic_id)
+            .ok_or_else(|| anyhow!(format!("Can not find epic id {epic_id:?}")))?
+            .status = status;
+        self.database.write_db(&db_state)?;
+
+        Ok(())
+    }
+
+    pub fn update_story_status(&self, story_id: u32, status: Status) -> Result<()> {
+        let mut db_state = self.read_db()?;
+        db_state
+            .stories
+            .get_mut(&story_id)
+            .ok_or_else(|| anyhow!(format!("Can not find story id {story_id:?}")))?
+            .status = status;
+        self.database.write_db(&db_state)?;
+
+        Ok(())
+    }
+}
 
 trait Database {
     fn read_db(&self) -> Result<DBState>;
